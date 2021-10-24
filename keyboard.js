631bd7e385a0269b6a7de27d11817b321950b93e
Tony Tang <tony.tang.developer@gmail.com>
2021-10-24 15:28:23 +0200

    added delay

--- keyboard.js
+++ keyboard.js
@@ -1,6 +1,6 @@
 import ks from 'node-key-sender'
 
-export function writeEvent(text) {
+export async function writeEvent(text) {
     const textArray = text.split("")
     const formattedTextArray = textArray.map(char => {
         if (char === " ") {
@@ -11,5 +11,17 @@ export function writeEvent(text) {
 
         return char
     })
-    ks.sendKeys(formattedTextArray)
+
+    for (let index = 0; index < formattedTextArray.length; index++) {
+        const key = formattedTextArray[index]
+        await sleep(ks.sendKey, key)
+    }
+}
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+async function sleep(fn, ...args) {
+    await timeout(500)
+    return fn(...args)
 }
